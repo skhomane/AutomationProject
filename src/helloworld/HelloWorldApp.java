5d3344b40072249d70cf8f6aff61547e05796e2f
skhomane <sachin.khomane@gmail.com>
2016-10-29 16:07:07 +0530

    Again added HelloWorldApp.java file.

---
+++ src/helloworld/HelloWorldApp.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class HelloWorldApp {
+
+	public static void main(String[] args) {
+		System.out.println("This is helloworld program and should be run on jenkins");
+	}
+			
+	
+}
